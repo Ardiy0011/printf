85dd518fe445dde49c85f63c95efcac9f1b62562
Ardiy <you@example.com>
2023-03-26 11:04:35 -0700

    retrofitted printf function to printing single characters,strings, integers and symbols

--- ourprintf.c
+++ ourprintf.c
@@ -12,48 +12,48 @@
  */
 int _printf(const char *format, ...)
 {
-    va_list arg_list;
-    int display = 0;
-    const char *p;
+va_list arg_list;
+int display = 0;
+const char *p;
 
-    va_start(arg_list, format);
+va_start(arg_list, format);
 
-    for (p = format; *p != '\0'; p++)
-    {
-        if (*p != '%')
-        {
-            putchar(*p);
-            display++;
-            }
-            else
-            {
-                p++;
-                switch (*p)
-                {
-                    case 'c':
-                    {
-                    char c = va_arg(arg_list, int);
-                    putchar(c);
-                    display++;
-                    break;
-                }
-                case 's':
-                {
-                    const char *s = va_arg(arg_list, const char *);
-                    display += printf("%s", s);
-                    break;
-                }
-                case '%':
-                {
-                    putchar('%');
-                    display++;
-                    break;
-                }
-            }
-        }
-    }
-    
-    va_end(arg_list);
-    
-    give_us(display);
+for (p = format; *p != '\0'; p++)
+{
+if (*p != '%')
+{
+putchar(*p);
+display++;
+}
+else
+{
+p++;
+switch (*p)
+{
+case 'c':
+{
+char c = va_arg(arg_list, int);
+putchar(c);
+display++;
+break;
+}
+case 's':
+{
+const char *s = va_arg(arg_list, const char *);
+display += printf("%s", s);
+break;
+}
+case '%':
+{
+putchar('%');
+display++;
+break;
+}
+}
+}
+}
+
+va_end(arg_list);
+
+give_us(display);
 }
