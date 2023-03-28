2a23f17e302c16dac73f644e05c07e5cd894b891
Ardiy <you@example.com>
2023-03-27 17:54:14 -0700

    switch statement solve to bug for main

--- printf.c
+++ printf.c
@@ -21,32 +21,31 @@ int _printf(const char *format, ...)
 
 i = 0;
 while (format && format[i] != '\0') {
-    switch (format[i]) {
-        case '%':
-            print_buffer(buffer, &buff_ind);
-            flags = get_flags(format, &i);
-            width = get_width(format, &i, list);
-            precision = get_precision(format, &i, list);
-            size = get_size(format, &i);
-            ++i;
-            printed = handle_print(format, &i, list, buffer, flags, width, precision, size);
-            if (printed == -1)
-                return (-1);
-            printed_chars += printed;
-            break;
-        default:
-            buffer[buff_ind++] = format[i];
-            if (buff_ind == BUFF_SIZE) {
-                print_buffer(buffer, &buff_ind);
-            }
-            printed_chars++;
-            break;
-    }
-    i++;
+	if (format[i] != '%') {
+		buffer[buff_ind++] = format[i];
+		if (buff_ind == BUFF_SIZE) {
+			print_buffer(buffer, &buff_ind);
+		}
+		printed_chars++;
+	} else {
+		print_buffer(buffer, &buff_ind);
+		flags = get_flags(format, &i);
+		width = get_width(format, &i, list);
+		precision = get_precision(format, &i, list);
+		size = get_size(format, &i);
+		i++;
+		printed = handle_print(format, &i, list, buffer, flags, width, precision, size);
+		if (printed == -1) {
+			return (-1);
+		}
+		printed_chars += printed;
+	}
+	i++;
 }
 print_buffer(buffer, &buff_ind);
 va_end(list);
 return (printed_chars);
+}
 
 
 /**
