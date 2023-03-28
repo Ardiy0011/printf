a01970875af3d1c4db26bfa66337cfbe766d6a19
Ardiy <you@example.com>
2023-03-27 18:06:24 -0700

    switch statement solve to bug for main

--- printf.c
+++ printf.c
@@ -18,42 +18,42 @@ int _printf(const char *format, ...)
 		return (-1);
 
 	va_start(list, format);
-
-i = 0;
-while (format && format[i] != '\0')
-{
-	if (format[i] != '%')
-{
-		buffer[buff_ind++] = format[i];
-		if (buff_ind == BUFF_SIZE)
-
-{
+	
+	
+	i = 0;
+	while (format && format[i] != '\0')
+	{
+		if (format[i] != '%')
+		{
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
+			/* write(1, &format[i], 1);*/
+			printed_chars++;
+		}
+		else
+		{
 			print_buffer(buffer, &buff_ind);
-}
-		printed_chars++;
-}
-else
-{
-		print_buffer(buffer, &buff_ind);
-		flags = get_flags(format, &i);
-		width = get_width(format, &i, list);
-		precision = get_precision(format, &i, list);
-		size = get_size(format, &i);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+			++i;
+			printed = handle_print(format, &i, list, buffer,
+				flags, width, precision, size);
+			if (printed == -1)
+				return (-1);
+			printed_chars += printed;
+		}
 		i++;
-		printed = handle_print(format, &i, list, buffer, flags, width, precision, size);
-		if (printed == -1)
-{
-			return (-1);
-}
-		printed_chars += printed;
 	}
-	i++;
-}
-print_buffer(buffer, &buff_ind);
-va_end(list);
-return (printed_chars);
-}
 
+	print_buffer(buffer, &buff_ind);
+
+	va_end(list);
+
+	return (printed_chars);
+}
 
 /**
  * print_buffer - Prints the contents of the buffer if it exist
